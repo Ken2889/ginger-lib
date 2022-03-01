2394bc5be04300dcccd6a7a15ca3272d8db138a0
DanieleDiBenedetto <daniele@horizenlabs.io>
2022-03-01 09:47:36 +0100

    Added some utility functions to LinearCombination struct

--- algebra/src/groups/linear_combination.rs
+++ algebra/src/groups/linear_combination.rs
@@ -1,30 +1,66 @@
 use super::Group;
+use crate::fields::Field;
 
 /// Generic struct of a formal linear combination
-pub struct LinearCombination<G: Group>
+pub struct LinearCombination<'a, G: Group>
 {
-    pub items: Vec<(G::ScalarField, G)>
+    items: Vec<(G::ScalarField, &'a G)>
 }
 
-impl<G: Group> LinearCombination<G>
+impl<'a, G: Group> LinearCombination<'a, G>
 {
     /// Consturcts general LC
-    pub fn new(items: Vec<(G::ScalarField, G)>) -> Self {
-        LinearCombination {
+    pub fn new(items: Vec<(G::ScalarField, &'a G)>) -> Self {
+        Self {
+            items
+        }
+    }
+
+    /// Constructs empty LC
+    pub fn empty() -> Self {
+        Self {
+            items: vec![]
+        }
+    }
+
+    /// Get LC length
+    pub fn length(&self) -> usize {
+        self.items.len()
+    }
+
+    /// Constructs LC where the coefficients are the powers of 'batching_val'
+    pub fn new_from_val(batching_val: &G::ScalarField, terms: Vec<&'a G>) -> Self {
+        let mut coeff = G::ScalarField::one();
+        let items = if terms.is_empty() {
+            vec![]
+        } else {
+            let mut items = vec![(coeff, terms[0])];
+            items.append(&mut terms
+                .into_iter()
+                .skip(1)
+                .map(|term| {
+                    coeff *= batching_val;
+                    (coeff, term)
+                })
+                .collect());
+            items
+        };
+
+        Self {
             items
         }
     }
 
     /// Add term to LC
-    pub fn push(&mut self, coeff: G::ScalarField, item: G) {
+    pub fn push(&mut self, coeff: G::ScalarField, item: &'a G) {
         self.items.push((coeff, item))
     }
 
     /// Combine LC
-    pub fn combine(&self) -> G {
+    pub fn combine(self) -> G {
         let mut combined = G::zero();
-        for (coeff, item) in self.items.iter() {
-            combined += &(item.clone() * coeff)
+        for (coeff, item) in self.items.into_iter() {
+            combined += &(item.clone() * &coeff)
         }
         combined
     }
