f7c9d7e3907adcf834d847e60f6d3ad8aa280e72
nicholas-mainardi <mainardinicholas@gmail.com>
2023-01-18 10:45:20 +0100

    Avoid running marlin & poly-commit tests twice in CI

--- ci/tests/022-cargo-test.sh
+++ ci/tests/022-cargo-test.sh
@@ -3,4 +3,4 @@
 
 set -xeo pipefail
 
-cargo $CARGOARGS test --workspace --all-features --exclude "r1cs-std" --exclude "proof-systems"
+cargo $CARGOARGS test --workspace --all-features --exclude "r1cs-std" --exclude "proof-systems" --exclude "marlin" --exclude "poly-commit"
