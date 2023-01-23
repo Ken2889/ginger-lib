6d9c288c7d047437d9872bec201f9e8d94b2bb82
la10736 <michele.damico@gmail.com>
2023-01-23 10:04:03 +0100

    PS-231: fixed lint advisories and prevent arbitrary code execution

--- ci/tests/026-cargo-audit.sh
+++ ci/tests/026-cargo-audit.sh
@@ -5,9 +5,12 @@ echo -e "Start cargo audit\n"
 
 if [ "$CARGO_AUDIT_EXIT_ON_ERROR" = "false" ]; then
   echo -e "\nCargo audit error disabled"
+elif [ "$CARGO_AUDIT_EXIT_ON_ERROR" != "true" ]; then
+  echo -e "\nERROR: CARGO_AUDIT_EXIT_ON_ERROR should be only true|false and not '$CARGO_AUDIT_EXIT_ON_ERROR'"
+  exit 1
 fi
 
 # Ok: if we want to ignore the `cargo audit` fail but we would see the report
 # we use $CARGO_AUDIT_EXIT_ON_ERROR set to false. In this case we ignore the
 # `cargo audit` output value and we return always `true`.
-cargo audit || ! `$CARGO_AUDIT_EXIT_ON_ERROR`
+cargo audit || ! eval "$CARGO_AUDIT_EXIT_ON_ERROR"
